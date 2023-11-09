cfe7f8027a188feaa62c3da9044955512a946f32
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-09 09:31:49 +0100

    implement createWallet

--- backend/src/main/java/com/annemariel/backend/wallet/WalletController.java
+++ backend/src/main/java/com/annemariel/backend/wallet/WalletController.java
@@ -1,9 +1,11 @@
 package com.annemariel.backend.wallet;
 
+import com.annemariel.backend.wallet.dto.WalletRequestDto;
 import com.annemariel.backend.wallet.dto.WalletResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +27,16 @@ public class WalletController {
     @GetMapping("/{id}")
     public ResponseEntity<WalletResponseDto> findById(@PathVariable String id) {
         return ResponseEntity.ok(walletService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<WalletResponseDto> createWallet(@RequestBody WalletRequestDto dto) {
+        WalletResponseDto wallet = walletService.createWallet(dto);
+        return ResponseEntity.created(URI.create("/api/wallets/" + wallet.id())).body(wallet);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WalletResponseDto> updateWallet(@PathVariable String id, @RequestBody WalletRequestDto dto) {
+        return ResponseEntity.ok(walletService.updateWallet(id, dto));
     }
 }
--- backend/src/main/java/com/annemariel/backend/wallet/WalletService.java
+++ backend/src/main/java/com/annemariel/backend/wallet/WalletService.java
@@ -1,6 +1,8 @@
 package com.annemariel.backend.wallet;
 
+import com.annemariel.backend.user.User;
 import com.annemariel.backend.user.UserRepository;
+import com.annemariel.backend.wallet.dto.WalletRequestDto;
 import com.annemariel.backend.wallet.dto.WalletResponseDto;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +29,20 @@ public class WalletService {
 
     public WalletResponseDto findById(String id) {
         return walletRepository.findById(id).map(a -> new WalletResponseDto(a.getId(), a.getName(), a.getBalance())).orElseThrow();
+    }
+
+    public WalletResponseDto createWallet(WalletRequestDto dto) {
+        User user = userRepository.findById(dto.userId()).orElseThrow();
+        Wallet acc = new Wallet(dto.name(), dto.balance(), user);
+        walletRepository.save(acc);
+        return new WalletResponseDto(acc.getId(), acc.getName(), acc.getBalance());
+    }
+
+    public WalletResponseDto updateWallet(String id, WalletRequestDto dto) {
+        Wallet wallet = walletRepository.findById((id)).orElseThrow();
+        wallet.setName(dto.name());
+        wallet.setBalance(dto.balance());
+        walletRepository.save(wallet);
+        return new WalletResponseDto(wallet.getId(), wallet.getName(), wallet.getBalance());
     }
 }
